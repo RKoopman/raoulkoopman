56e46b37c8489ef81970e4bf8db5c702eca6ff80
RKoopman <raoul.koopman@gmail.com>
2017-02-06 14:17:10 -0500

    comment out actionmailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
-class ApplicationMailer < ActionMailer::Base
-  default from :"raoul.koopman@gmail.com "
-  layout 'mailer'
-end
+# class ApplicationMailer < ActionMailer::Base
+#   default from :"raoul.koopman@gmail.com "
+#   layout 'mailer'
+# end
