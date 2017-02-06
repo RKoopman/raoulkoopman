fc9d8bbaf9fb1d9ea0d21b3cd30715aabcaa710a
RKoopman <raoul.koopman@gmail.com>
2017-02-06 17:03:23 -0500

    env vars

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,8 +37,8 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
-    user_name:            EMAIL_ADDRESS,
-    password:             EMAIL_PASS,
+    user_name:            'EMAIL_ADDRESS',
+    password:             'EMAIL_PASS',
     authentication:       :login,
     enable_starttls_auto: true
 
