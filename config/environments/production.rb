3ff8394c0fc4dd2093c67ecdb4dfba305ab2e405
RKoopman <raoul.koopman@gmail.com>
2017-02-06 21:27:58 -0500

    setup pro action_mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,14 +64,23 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
-  # Use a real queuing backend for Active Job (and separate queues per environment)
-  # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "KoopmanDevs_#{Rails.env}"
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
 
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            ENV["EMAIL_ADDRESS"],
+    password:             ENV["EMAIL_PASS"],
+    authentication:       :login,
+    enable_starttls_auto: true
+
+      }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
